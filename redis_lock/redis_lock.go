5c30bc546a9653f5487e291cfa3a281cdf3f7be2
陈梓淳 <2941125807@qq.com>
2024-11-29 22:16:40 +0800

    feat: add acquire lock logic

--- redis_lock/redis_lock.go
+++ redis_lock/redis_lock.go
@@ -1,13 +1,21 @@
 package redis_lock
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"redis_distributed_lock/utils"
+	"time"
 )
 
 var ErrLockAcquiredByOthers = errors.New("lock is acquired by others")
 
-func IsAcquiredErr(err error) bool {
+const (
+	redisLockKeyPrefix = "REDIS_LOCK_"
+	tickerDuration     = 50
+)
+
+func isRetryableErr(err error) bool {
 	return errors.Is(err, ErrLockAcquiredByOthers)
 }
 
@@ -31,4 +39,63 @@ func NewRedisLock(key string, client *Client, opts ...LockOption) *RedisLock {
 
 	repairLock(&lock.LockOptions)
 	return &lock
+}
+
+func (lock *RedisLock) getLockKey() string {
+	return redisLockKeyPrefix + lock.key
+}
+
+func (lock *RedisLock) tryLock(ctx context.Context) error {
+	resp, err := lock.client.SetNX(ctx, lock.getLockKey(), lock.token, lock.expireSeconds)
+	if err != nil {
+		return err
+	}
+	if resp != 1 {
+		return fmt.Errorf("resp: %d, err: %w", resp, ErrLockAcquiredByOthers)
+	}
+	return nil
+}
+
+func (lock *RedisLock) blockingLock(ctx context.Context) error {
+	timeLimit := time.After(time.Duration(lock.blockWaitingSeconds) * time.Second)
+	ticker := time.NewTicker(time.Duration(tickerDuration) * time.Millisecond)
+    defer ticker.Stop()
+
+    for range ticker.C {
+        select {
+        case <-ctx.Done():
+            return fmt.Errorf("lock failed, ctx timeout, error: %w", ctx.Err())
+        case <-timeLimit:
+            return fmt.Errorf("block waiting timeout, error: %w", ErrLockAcquiredByOthers)
+        default:
+        }
+
+        err := lock.tryLock(ctx)
+        if err == nil {
+            return nil
+        }
+
+        if !isRetryableErr(err) {
+            return err
+        }
+    }
+
+    return nil
+}
+
+func (lock *RedisLock) Lock(ctx context.Context) error {
+	err := lock.tryLock(ctx)
+	if err == nil {
+		return nil
+	}
+
+	if !lock.isBlock {
+		return err
+	}
+
+	if !isRetryableErr(err) {
+		return err
+	}
+
+	return lock.blockingLock(ctx)
 }
